ca271d0efa8ef63851cd00c0dbd64093b983d3f3
aparrish <aparrish@nyu.edu>
2011-01-30 02:17:18 +0000

    added support for stats.json (required reworking some cache stuff)

--- mimetree_web.py
+++ mimetree_web.py
@@ -27,6 +27,7 @@ import urllib
 import json
 import redis
 import bsoup_parse
+import mateman
 
 from BeautifulSoup import BeautifulStoneSoup
 
@@ -44,6 +45,7 @@ class Application(tornado.web.Application):
 			(r"/", MainHandler),
 			(r"/friends.json", FriendsJSONHandler),			
 			(r"/auth/login", AuthLoginHandler),
+			(r"/stats.json", StatsHandler)
 		]
 		settings = dict(
 			cookie_secret="12oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
@@ -62,44 +64,80 @@ class BaseHandler(tornado.web.RequestHandler):
 		user_json = self.get_secure_cookie("user")
 		if not user_json: return None
 		return tornado.escape.json_decode(user_json)
-
-class MainHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
-	@tornado.web.authenticated
-	@tornado.web.asynchronous
-	def get(self):
-		self.render("index.html")
-
-class FriendsJSONHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
-	@tornado.web.authenticated
-	@tornado.web.asynchronous
-	def get(self):
-		self.set_header('Content-Type', 'application/json')
-		r = redis.Redis(host=options.redis_host, port=options.redis_port, db=0)
+	def get_redis_conn(self):
+		return redis.Redis(host=options.redis_host, port=options.redis_port, db=0)
+	def get_graph_data(self):
+		r = self.get_redis_conn()
 		key = "%s:friends" % (self.current_user['access_token'])
 		data = r.get(key)
 		if data:
 			logging.info('got data from cache')
-			self.write(data)
-			self.finish()
+			return data
 		else:
 			logging.info('cache miss')
-			http = tornado.httpclient.AsyncHTTPClient()
-			http.fetch('https://api.facebook.com/method/fql.query?' + \
+			http = tornado.httpclient.HTTPClient()
+			response = http.fetch('https://api.facebook.com/method/fql.query?' + \
 				urllib.urlencode({'access_token': self.current_user['access_token'],
-					'query': 'select uid, first_name, last_name, sex, current_location, activities, interests, music, tv, movies, books, work_history, education_history from user where uid in (select uid2 from friend where uid1 = me()) order by rand() limit 200'}),
-				callback=self._on_response, request_timeout=120.0)
+					'query': 'select uid, first_name, last_name, sex, current_location, activities, interests, music, tv, movies, books, work_history, education_history from user where uid in (select uid2 from friend where uid1 = me()) order by rand() limit 200'}), request_timeout=120.0)
+			logging.info("response %s" % str(response))
+			if response.error:
+				return None
+			soup = BeautifulStoneSoup(response.body)
+			if soup.find('error_code'):
+				return None
+			rawjson = json.dumps(bsoup_parse.parse_soup(soup))
+			logging.info('setting cache key')
+			r.setex(key, rawjson, 86400)
+			# clear graph stats if we've freshly loaded friends
+			r.delete("%s:graph_stats" % self.current_user['access_token'])
+			return rawjson
 
-	def _on_response(self, response):
-		logging.error("response %s" % str(response))
-		if response.error: raise tornado.web.HTTPError(500)
-		soup = BeautifulStoneSoup(response.body)
-		rawjson = json.dumps(bsoup_parse.parse_soup(soup))
+	def get_graph_stats(self):
+		r = self.get_redis_conn()
+		key = "%s:graph_stats" % (self.current_user['access_token'])
+		data = r.get(key)
+		if data:
+			logging.info('got graph stats from cache')
+			return json.loads(data)
+		else:
+			logging.info('graph stats cache miss')
+			friend_stats = self.get_graph_data()
+			friend_data = json.loads(friend_stats)
+			crunched = mateman.crunchNumbers(friend_data)
+			r.setex(key, json.dumps(crunched), 86400)
+			return crunched
+
+class MainHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
+	@tornado.web.authenticated
+	def get(self):
+		self.render("index.html")
+
+class StatsHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
+	@tornado.web.authenticated
+	def get(self):
+		friends = json.loads(self.get_graph_data())
+		stats = self.get_graph_stats()
+		fbid = self.get_argument("uid", None)
+		if fbid:
+			if fbid not in friends:
+				raise tornado.web.HTTPError(404)
+			logging.info("user: %s" % str(friends[fbid]))
+			logging.info("stats: %s (%s)" % (str(stats), str(type(stats))))
+			self.set_header('Content-Type', 'application/json')
+			self.write(json.dumps(mateman.getStats(friends[fbid], stats)))
+		else:
+			raise tornado.web.HTTPError(400)
+		
+
+class FriendsJSONHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
+	@tornado.web.authenticated
+	def get(self):
+		self.set_header('Content-Type', 'application/json')
+		rawjson = self.get_graph_data()
+		if rawjson is None:
+			# couldn't get json, probably auth problem
+			self.redirect('/auth/login')
 		self.write(rawjson)
-		logging.error('setting cache key')
-		r = redis.Redis(host=options.redis_host, port=options.redis_port, db=0)
-		r.setex("%s:friends" % self.current_user['access_token'], rawjson, 86400)
-		#logging.error(response.body)
-		self.finish()
 
 class AuthLoginHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
 	@tornado.web.asynchronous
