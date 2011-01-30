e4b9a8acd3edbfc9923d3df3ac3d2b0b60c74a09
aparrish <aparrish@nyu.edu>
2011-01-30 16:25:19 +0000

    fixed bug wherein monosyllabic names ending in all vowels (e.g. "Bree", "Joe") would result in weirdly cut-off names (i.e. "Bree" + "Catherine" = either "Ca" or "therine")

--- portnameteau.py
+++ portnameteau.py
@@ -6,15 +6,19 @@ def vtoc_idx(s):
 	if match_obj:
 		return match_obj.start() + 1
 	else:
-		return 0
+		return None
 
 def portnameteau(name_list):
 	random.shuffle(name_list)
 	s1 = name_list[0]
 	s2 = name_list[1]
 	end_s1 = vtoc_idx(s1)
+	if end_s1 is None:
+		end_s1 = len(s1)
 	start_s2 = vtoc_idx(s2)
-	return s1[:end_s1] + s2[start_s2:]
+	if start_s2 is None:
+		start_s2 = 0
+	return (s1[:end_s1] + s2[start_s2:]).title()
 
 if __name__ == '__main__':
 	first_names = ['Nick','Lisa','Nathan','Peter','Jason','Yasmin','Lesley','Florica','Eric','Tom','Mike','Hondo','Jeff','Scott','Elim','Caleb','Brooke','Mooshir','Sinan','Matthew','Rob','Michael','Corey','Kara','Catherine','Caleb','Anderson','Alejandro','Shelly','Tymm','Steven','Jelani','Sonaar','Jason','Kevin','Hyeki','Yan','Andrea','Steven','Charlotte','Andrew','Fiction','Diego','Kalle','John','Amy','Kacie','Nathan','Adam','Marina','Eldin','Mitch','Steven','Michael','Jeff','Corrine','Viceroy','Neilson','Rory','Ozge','Rucyl','Patricia','Jason','Ruth','Michael','Adam','J.R.', 'Amelia','ChoRong','Kate','Aaron','Doug','Robert','Michael','Calista','Brendan','Emily','Rachelle','Kristin','Celina','Claire','Jaymes','Benjamin','Joshua','Scott','Ohad','Preston','Jason','Samuel','Scott','Tim','Sanjay','Suzan','Derek','Benedetta','Mike','Heather','Jaewook','Lisa','Lauren','Philippe','Vikram','Evan','Casey','Emery','Daniel','Patty','Fitzgorilla','Edward','John','Adam','Steven','Mushon','Emma','Lee-Sean','Peter','Alex','Bree','Eugene','Nathan','Kevin','Leah','Gian','Yonatan','Jorge','Max','Tom','Joora','Josh','Ameya','Nicholas','Greg','Jody','Charles','John','Brian','Zeeshan','Piama','Michael','Brian','Ithai','Denisse','Carolina','Xiaoyang','Shawn','Derek','Ben','Luke','Preston','Eric','Ilan','Timothy','Brenton','Tonio','Marco','Kati','Tristan','Ben','Robert','Shinyoung','George','Yasser','Oscar','Joseph','Zannah','Lois','Susan','Sabrina','Chris']
@@ -27,3 +31,5 @@ if __name__ == '__main__':
 		last_name2 = random.choice(last_names)
 		print "%s %s + %s %s = %s %s" % (first_name1, last_name1, first_name2, last_name2, portnameteau([first_name1, first_name2]), portnameteau([last_name1, last_name2]))
 
+	print portnameteau(['Bree', 'Catherine'])
+	print portnameteau(['Bree', 'Joe'])
